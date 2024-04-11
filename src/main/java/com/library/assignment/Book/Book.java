98bdacb9c99fbf3d0d16f7f2e68d912835a3d887
Mustafacz <145360760+Mustafacz@users.noreply.github.com>
2024-04-11 17:51:42 +0200

    Add files via upload

---
+++ src/main/java/com/library/assignment/Book/Book.java
@@ -0,0 +1,37 @@
+package com.library.assignment.Book;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false)
+    private LocalDate releaseDate;
+    @Column
+    private boolean borrowed = false;
+
+    @Column
+    private LocalDate borrowedDate;
+
+    @Column
+    private Long loanerId;
+
+}
